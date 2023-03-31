ae4ba83b21c1107264059e54ebbc5448e38abc38
plebg <plebeiusgaragicus@gmail.com>
2023-03-31 12:06:42 -0700

    init

---
+++ app.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json()); // Add this to parse JSON request bodies
+
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'public/index.html'));
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+// const host = process.env.IP_ADDRESS
+// console.log(host)
+
+// app.listen(port, host, () => {
+//     console.log(`Server is running on http://${host}:${port}`);
+// });
---
+++ public/index.js
@@ -0,0 +1,18 @@
+// import config from './config.js';
+
+
+const createInvoiceButton = document.getElementById('createInvoice');
+
+createInvoiceButton.addEventListener('click', () => {
+    console.log("createInvoiceButton clicked");
+    alert("createInvoiceButton clicked");
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM loaded");
+});
+
+
+// window.addEventListener('focus', () => {
+// });
